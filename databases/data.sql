30ad39545a6a866ced9060336ac63c368551a026
Harena0712 <harenasarmnk@gmail.com>
2026-04-29 10:49:31 +0300

    Données etudiants

--- databases/data.sql
+++ databases/data.sql
@@ -53,3 +53,25 @@ CREATE TABLE users(
 );
 
 INSERT INTO users (email, password) VALUES ('admin@sysinfo.mg', '123456');
+
+INSERT INTO etudiants (nom, prenom, date_naisssance) VALUES
+('Rakoto', 'Jean', '2001-03-15'),
+('Rabe', 'Marie', '2000-07-22'),
+('Andriamihaja', 'Luc', '2002-01-10'),
+('Rasoa', 'Sophie', '2001-11-05'),
+('Randria', 'Kevin', '1999-06-18'),
+('Rakotomalala', 'Nina', '2003-09-27'),
+('Razanajatovo', 'Paul', '2000-12-14'),
+('Raharisoa', 'Clara', '2002-05-09'),
+('Andrianina', 'Mickael', '2001-08-30'),
+('Ravelomanana', 'Julie', '2000-02-17'),
+('Ratsimba', 'Hery', '1998-10-25'),
+('Rakotondrabe', 'Fania', '2003-04-12'),
+('Rafanomezantsoa', 'Lova', '2001-07-03'),
+('Ramanandraibe', 'Aina', '2002-06-21'),
+('Razafindrakoto', 'Tiana', '1999-09-11'),
+('Rabetsimandranto', 'Mialy', '2000-01-29'),
+('Andriantsitohaina', 'Toky', '2001-12-08'),
+('Raharimalala', 'Hanta', '2002-03-26'),
+('Rakotovao', 'Fitia', '2000-05-16'),
+('Rasoanandrasana', 'Tahina', '2003-08-04');
